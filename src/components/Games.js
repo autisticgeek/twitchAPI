66c0538d92f29216d8dbee45742fe2073f15cc05
Steven Blakely <geek@autisticgeek.com>
2019-08-06 22:33:29 -0600

    css transitions

--- src/components/Games.js
+++ src/components/Games.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import {Link} from 'react-router-dom';
 import api from '../api'
 
 
@@ -17,9 +18,9 @@ function Games() {
         <h1 style={{textAlign: 'center'}}>Most Popular Games</h1>
         <ol style={{display: 'flex', flexWrap: 'wrap', justifyContent:'space-around', listStyle:'none'}}>
             {games.map((game, i) => (
-                <li key={i} style={{display:'flex', flexDirection:'column', alignItems: 'center', marginBottom: '1rem'}}>
-                    <img style={{borderRadius:'15px'}} src={game.box_art_url.replace('{width}', '300').replace('{height}', '300')} alt={game.name} />
-                    <p style={{maxWidth: '300px', textAlign:'center'}}>{i+1}. {game.name}</p>
+                <li key={i} style={{display:'flex', flexDirection:'column', alignItems: 'center', marginBottom: '1rem', minWidth: '300px'}}>
+                    <Link className='hover'><img style={{borderRadius:'15px'}} src={game.box_art_url.replace('{width}', '200').replace('{height}', '200')} alt={game.name} />
+                    <p style={{maxWidth: '200px', textAlign:'center'}}>{i+1}. {game.name}</p></Link>    
                 </li>
             ))}
         </ol>
